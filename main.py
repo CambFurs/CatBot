1daa5a1b057a10d49a937d3eaf1c94b775859b60
Douwe van Gijn <douwevangijn@hotmail.com>
2024-06-01 14:13:31 +0100

    added welcome messages, main chat alerts

--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 import tomllib
 import datetime
 from dataclasses import dataclass
+from telegram.constants import ChatMemberStatus
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, ChatJoinRequestHandler
+from telegram.ext import Application, ApplicationBuilder, ContextTypes, CommandHandler, ChatJoinRequestHandler, ChatMemberHandler
 
 # Configuration ################################################################
 
@@ -29,24 +30,47 @@ async def get_admins(bot,group_id=CONFIG.main_group_id):
     chat_members = await bot.get_chat_administrators(CONFIG.main_group_id)
     return {chat_member.user.id for chat_member in chat_members}
 
-async def respond(update: Update, context: ContextTypes.DEFAULT_TYPE, message: str):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+async def respond(update: Update, context: ContextTypes.DEFAULT_TYPE, message: str, **kwargs) -> None:
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=message, **kwargs)
 
-async def respond_success(update: Update, context: ContextTypes.DEFAULT_TYPE, message: str):
+async def respond_success(update: Update, context: ContextTypes.DEFAULT_TYPE, message: str) -> None:
     await respond(update, context, f"✅ {message}")
 
-async def respond_error(update: Update, context: ContextTypes.DEFAULT_TYPE, message: str):
+async def respond_error(update: Update, context: ContextTypes.DEFAULT_TYPE, message: str) -> None:
     await respond(update, context, f"❌ {message}")
 
 # Commands #####################################################################
 
-COMMANDS = []
+COMMANDS : list[str] = []
 
+async def initialize(app: Application) -> None:
+    await app.bot.send_message(chat_id=CONFIG.admin_group_id, text="🟢 CatBot started")
+
+async def finalize(app: Application) -> None:
+    await app.bot.send_message(chat_id=CONFIG.admin_group_id, text="🆘 CatBot stopped")
+
+async def chat_member_updated(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    if update.chat_member.chat.id != CONFIG.waiting_room_group_id:
+        return
+    old = update.chat_member.old_chat_member
+    new = update.chat_member.new_chat_member
+    if old.status==ChatMemberStatus.LEFT and new.status==ChatMemberStatus.MEMBER:
+        print("join event")
+        await context.bot.send_message(chat_id=CONFIG.admin_group_id, text=
+            f"🆕 {new.user.first_name} {new.user.last_name} (@{new.user.username})")
+        await context.bot.send_message(chat_id=CONFIG.waiting_room_group_id, text=
+            f"Hi {new.user.first_name}! An admin will be with you shortly to get you in the main chat.\n\nPlease read the rules at rules.cambfurs.co.uk and let us know and whether you agree.")
 
 async def cmd_start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """/start: prints help message"""
+    """/start: initiate a CatBot conversation"""
+    if update.message.chat.type!="private":
+        return
+    admin_set = await get_admins(context.bot)
+    if update.message.from_user.id not in admin_set:
+        await respond(update, context, "Meow!")
+        return
     command_docs = '\n'.join([cmd.__doc__ for cmd in COMMANDS])
-    await respond(update, context, "Hewwo! I'm Catbot! Here's what I can do:\n"+command_docs)
+    await respond(update, context, f"Hewwo! I'm Catbot! These are the things I can do:\n{command_docs}", protect_content=True)
 COMMANDS.append(cmd_start)
 
 
@@ -58,10 +82,10 @@ async def cmd_say(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
     admin_set = await get_admins(context.bot)
     if update.message.from_user.id not in admin_set:
-        await respond_error(update,context,f"Only admins may use this command")
+        await respond_error(update,context,"Only admins may use this command")
         return
 
-    if update.message.reply_to_message==None:
+    if update.message.reply_to_message is None:
         print(update.message)
         await respond_error(update,context,"Please respond to the message you wish to send")
         return
@@ -74,7 +98,7 @@ COMMANDS.append(cmd_say)
 APPROVED_JOIN_REQUESTS = {}
 
 async def cmd_approve(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """/approve @username: send personalized invite link to user"""
+    """/approve @username: create invite link for user"""
 
     if not( update.message.chat.id==CONFIG.waiting_room_group_id ):
         return
@@ -91,7 +115,8 @@ async def cmd_approve(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Non
 
     global APPROVED_JOIN_REQUESTS
     if user in APPROVED_JOIN_REQUESTS:
-        print("TODO: revoke previous link for user")
+        await context.bot.revoke_chat_invite_link(CONFIG.main_group_id, APPROVED_JOIN_REQUESTS[user])
+        del APPROVED_JOIN_REQUESTS[user]
 
     minutes_valid = 5
     invite_link = await context.bot.create_chat_invite_link(
@@ -106,40 +131,44 @@ COMMANDS.append(cmd_approve)
 
 
 async def join_request(update:Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    async def decline():
-        await context.bot.decline_chat_join_request(update.chat_join_request.chat.id, update.chat_join_request.from_user.id)
+    user_id = update.chat_join_request.from_user.id
+    chat_id = update.chat_join_request.chat.id
 
-    if update.chat_join_request.chat.id!=CONFIG.main_group_id:
+    if chat_id!=CONFIG.main_group_id:
         print("Received join request for chat other than main group. Declined")
-        await decline()
+        await context.bot.decline_chat_join_request(chat_id, user_id)
         return
 
-    username = f"@{update.chat_join_request.from_user.username}";
-    print(username)
+    username = f"@{update.chat_join_request.from_user.username}"
 
     global APPROVED_JOIN_REQUESTS
     if not( username in APPROVED_JOIN_REQUESTS and APPROVED_JOIN_REQUESTS[username] != update.chat_join_request.invite_link ):
-        print("User not in approved join requests. Declined")
-        await decline()
+        print(f"{username} not in approved join requests. Declined")
+        await context.bot.decline_chat_join_request(chat_id, user_id)
         return
 
     print(f"{username} successfully joined")
     del APPROVED_JOIN_REQUESTS[username]
-    await context.bot.approve_chat_join_request(update.chat_join_request.chat.id, update.chat_join_request.from_user.id)
-    await context.bot.revoke_chat_invite_link(update.chat_join_request.chat.id, update.chat_join_request.invite_link)
+    await context.bot.approve_chat_join_request(chat_id, user_id)
+    await context.bot.revoke_chat_invite_link(chat_id, update.chat_join_request.invite_link)
     await context.bot.send_message(CONFIG.main_group_id, text=f"Everyone welcome {username} to the chat!")
+    await context.bot.ban_chat_member(CONFIG.waiting_room_group_id, user_id)
     print(update)
 
 # Main #########################################################################
 
 def main() -> None:
-    app = ApplicationBuilder().token(CONFIG.bot_token).build()
+    app = ApplicationBuilder()\
+        .token(CONFIG.bot_token)\
+        .post_init(initialize)\
+        .post_stop(finalize)\
+        .build()
     app.add_handler(CommandHandler("start", cmd_start))
     app.add_handler(CommandHandler("say",   cmd_say))
     app.add_handler(CommandHandler("approve", cmd_approve))
+    app.add_handler(ChatMemberHandler(chat_member_updated, ChatMemberHandler.CHAT_MEMBER))
     app.add_handler(ChatJoinRequestHandler(join_request))
-    app.run_polling()
+    app.run_polling(allowed_updates=Update.ALL_TYPES)
 
 if __name__ == '__main__':
     main()
-
